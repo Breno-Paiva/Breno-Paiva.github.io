b0c8d1766d912c3eb13738f7adacf7ac401d07e8
Breno Paiva <breno.paiva93@gmail.com>
2017-08-11 18:10:14 -0400

    clean up code

--- space.js
+++ space.js
@@ -1,27 +1,4 @@
-// bonsai.run(document.getElementById('movie'), {
-//     code: function() {
-//       new Rect(10, 10, 100, 100)
-//         .addTo(stage)
-//         .attr('fillColor', 'green');
-//     },
-//     width: 500,
-//     height: 400
-//   });
-
-  // var screenWidth = 250;
-  // var screenHeight = 250;
-  //
-  // var circleShape = new Circle(-150, 150, 50).fill('teal').addTo(stage);
-  //
-  // var circleAnimation = new Animation('2s',{
-  //   x: 1200
-  // },{
-  //   repeat: 100
-  // })
-  //
-  // circleShape.animate(circleAnimation)
-
-  $('#navv').affix({
+$('#navv').affix({
     offset: {
       top: function () {
         return ($('.space-header').outerHeight())
